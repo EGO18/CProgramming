0e23167165dfef3323ed43bce2eb4952df881fd7
Evan Owens <eowens9516@floridapoly.edu>
2025-09-08 15:05:51 +0000

    Lists

---
+++ 04_lists/all_same.py
@@ -0,0 +1,15 @@
+# Get the list
+n = int(input("Enter the number of elements:"))
+elements = []
+
+while n:
+    elements.append(int(input()))
+    n -= 1
+print(elements)
+
+# Print true if all elements are the same
+# Print false otherwise
+if (len(elements < 2)):
+    print("True")
+else:
+    print(bool(elements.count(elements[0]) == len(elements)))
