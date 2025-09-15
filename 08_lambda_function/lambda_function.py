a2bd2301f59ae6521954652473f50ee56d0e82e5
Evan Owens <eowens9516@floridapoly.edu>
2025-09-15 18:00:33 +0000

    Lambda Function

---
+++ 08_lambda_function/lambda_function.py
@@ -0,0 +1,14 @@
+#Create a lambda function
+add = lambda x, y: x + y
+
+#Create subtract, multiply and divide
+
+subtract = lambda x, y: x - y
+multiply = lambda x, y: x * y
+divide = lambda x, y: x / y if y != 0 else "Error: Divison by zero"
+
+#Create a list of tuples
+inputs = [(1,3),(9,7),(10,8)]
+for x,y in inputs:
+    print(f"Inputs: {x}, {y}")
+    print(f"Add: {add(x,y)}")
