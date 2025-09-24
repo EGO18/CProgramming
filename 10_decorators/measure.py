dfc6e8c0c16a9ecb1456ab85da4c1f92e0048bdc
Evan Owens <eowens9516@floridapoly.edu>
2025-09-24 15:18:39 +0000

    Decorators

--- 10_decorators/measure.py
+++ 10_decorators/measure.py
@@ -1,7 +1,14 @@
 from time import sleep, time
-def measure(func, *args, **kwargs):
+def measure(func):
     def wrapper(*args,**kwargs):
         t = time() # get current time
         func() # call the received function
         print(func.__name__, "took:", time()-t)
     return wrapper
+
+@measure
+def f2(sleep_time = 0.1):
+    sleep(sleep_time)
+
+if __name__ == "__main__":
+    print("Running directly")
